f9a16691874dcb75d304e5fd3286e9f5e7d0bb5d
MdHassan997 <hassanmondal1900@gmail.com>
2024-04-30 17:19:30 +0530

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, request , send_from_directory
 import os
 app = Flask(__name__)
 
@@ -10,9 +10,21 @@ def home():
 def diet():
     return render_template('home.html')
 
-@app.route('/results')
+@app.route('/result', methods=['POST'])
 def mealplan():
-    return render_template('results.html')
+    if request.method == 'POST':
+        # Access form data
+        diet_type = request.form.get('diet')
+        disease = request.form.get('disease')
+        veg_non_veg = request.form.get('veg_non_veg')
+        nutrient = request.form.get('nutrient')
+        
+        # Process form data as needed
+        # For example, you can pass the form data to the results template
+        return render_template('results.html', diet=diet_type, disease=disease, veg_non_veg=veg_non_veg, nutrient=nutrient)
+    else:
+        # Handle GET requests to /result route if needed
+        return render_template('error.html', message='Method Not Allowed')
 
 
 
