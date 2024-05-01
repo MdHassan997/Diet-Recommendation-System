829aee195deec0637e044e6bdf35ec3124d6551e
MdHassan997 <hassanmondal1900@gmail.com>
2024-05-01 20:39:16 +0530

    update

--- app.py
+++ app.py
@@ -15,21 +15,21 @@ def home():
 def diet():
     return render_template('home.html')
 
-@app.route('/result', methods=['POST'])
-def mealplan():
-    if request.method == 'POST':
-        # Access form data
-        diet_type = request.form.get('diet')
-        disease = request.form.get('disease')
-        veg_non_veg = request.form.get('veg_non_veg')
-        nutrient = request.form.get('nutrient')
+# @app.route('/result', methods=['POST'])
+# def mealplan():
+#     if request.method == 'POST':
+#         # Access form data
+#         diet_type = request.form.get('diet')
+#         disease = request.form.get('disease')
+#         veg_non_veg = request.form.get('veg_non_veg')
+#         nutrient = request.form.get('nutrient')
         
-        # Process form data as needed
-        # For example, you can pass the form data to the results template
-        return render_template('results.html', diet=diet_type, disease=disease, veg_non_veg=veg_non_veg, nutrient=nutrient)
-    else:
-        # Handle GET requests to /result route if needed
-        return render_template('error.html', message='Method Not Allowed')
+#         # Process form data as needed
+#         # For example, you can pass the form data to the results template
+#         return render_template('results.html', diet=diet_type, disease=disease, veg_non_veg=veg_non_veg, nutrient=nutrient)
+#     else:
+#         # Handle GET requests to /result route if needed
+#         return render_template('error.html', message='Method Not Allowed')
     
 @app.route('/bmrForm',methods=["POST","GET"])
 def bmrCalculate():
